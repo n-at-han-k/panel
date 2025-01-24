3de78e211aa270c8612b071c49f908e7f425c80b
Nathan Kidd <nathanblenheimkidd@gmail.com>
2025-01-24 00:50:51 +0000

    added basic layout and nav components

---
+++ app/components/nav/top_component.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+class Nav::TopComponent < ViewComponent::Base
+end
---
+++ test/components/nav/drawer_component_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class Nav::DrawerComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(Nav::DrawerComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
