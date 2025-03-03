707413273cf5996a1a838001292626bf59db8e93
Nathan Kidd <nathanblenheimkidd@gmail.com>
2025-03-03 23:29:08 +0000

    slimmed down changed nodemon

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,14 +1,18 @@
-# Pin npm packages by running ./bin/importmap
-
 pin "application"
+
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "@rails/actiontext", to: "actiontext.esm.js"
+
+pin_all_from "app/javascript/initializers", under: "initializers"
+pin_all_from "app/javascript/lib", under: "lib"
+pin_all_from "app/javascript/channels", under: "channels"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin_all_from "app/javascript/helpers", under: "helpers"
+pin_all_from "app/javascript/models", under: "models"
+pin_all_from "vendor/javascript/languages", under: "languages"
 
 pin "bootstrap", to: "bootstrap.esm.js" # 5.3.2
 pin "@popperjs/core", to: "popperjs-core.esm.js" # @2.11.8
-pin "trix"
-pin "@rails/actiontext", to: "actiontext.esm.js"
 
-pin "@sqlite.org/sqlite-wasm", to: "@sqlite.org--sqlite-wasm.js" # @3.48.0
