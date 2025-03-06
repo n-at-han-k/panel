a9e58ab5cfbe81fbd1f50018fb0fd64b2d42becb
Nathan Kidd <nathanblenheimkidd@gmail.com>
2025-03-06 02:09:16 +0000

    .

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,12 +7,8 @@ pin "@rails/actiontext", to: "actiontext.esm.js"
 
 pin_all_from "app/javascript/initializers", under: "initializers"
 pin_all_from "app/javascript/lib", under: "lib"
-pin_all_from "app/javascript/channels", under: "channels"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin_all_from "app/javascript/helpers", under: "helpers"
-pin_all_from "app/javascript/models", under: "models"
-pin_all_from "vendor/javascript/languages", under: "languages"
 
 pin "bootstrap", to: "bootstrap.esm.js" # 5.3.2
 pin "@popperjs/core", to: "popperjs-core.esm.js" # @2.11.8
-
+pin "trix"
